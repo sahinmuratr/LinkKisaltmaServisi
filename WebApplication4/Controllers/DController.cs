6ae8dcf4a1ac5be4c48ae9778188804ddb5648d0
sahinmuratr <sahinmuratr@gmail.com>
2022-06-26 14:29:36 +0300

    SON

--- WebApplication4/Controllers/DController.cs
+++ WebApplication4/Controllers/DController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,18 +13,44 @@ namespace WebApplication4.Controllers
         // GET: D
         public ActionResult In(String I)
         {
-
-            Link link = new Link();
-
-            link.KisaLink = I;
-
-            link = link.UzunLinkGetir();
-            if (link.Count < 10)
+            try
             {
-                link.SayacArttir();
-                Response.Redirect(link.UzunLink);
+                Link link = new Link();
+
+                link.KisaLink = I;
+
+                link = link.UzunLinkGetir();
+                if (link.Count < 10)
+                {
+                    link.SayacArttir();
+                    Response.Redirect(link.UzunLink);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                string date = DateTime.Now.ToString();
+                string msg = ex.Message;
+                string trace = ex.StackTrace;
+                string helplink = ex.HelpLink;
+                string path = Server.MapPath("~/log/"+DateTime.Now.ToString("dd-MM-yyyy")+".txt");
+
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    sw.WriteLine("------------------------");
+                    sw.WriteLine("");
+                    sw.WriteLine("");
+                    sw.Write(date + " ");
+                    sw.Write(msg + " ");
+                    sw.Write(helplink + " ");
+                    sw.WriteLine(trace);
+                    sw.WriteLine("");
+                    sw.WriteLine("");
+                    sw.WriteLine("----------------------");
+                }
             }
             return View();
         }
     }
 }
+
